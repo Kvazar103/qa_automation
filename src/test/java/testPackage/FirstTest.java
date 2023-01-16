9c4903d32cb9e8ab56319ec0133f62f19ec428a4
Kvazar103 <nazar.voinarovych250@gmail.com>
2023-01-16 14:36:13 +0200

    Initial commit

---
+++ src/test/java/testPackage/FirstTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test //так позначаються тести
+    public void test(){
+        System.out.println("My first test");
+    }
+}
